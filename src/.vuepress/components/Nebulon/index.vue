28d2cced006619801f60dac6c57f389265fdd55c
pandegong <674948122@qq.com>
2023-05-15 19:15:40 +0800

    添加组件资源

---
+++ src/.vuepress/components/Nebulon/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <Teleport to="body">
+        <div id="container"></div>
+    </Teleport>
+</template>
+
+<script>
+export default {
+    name: "Nebulon",
+    components: {},
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    mounted() {
+
+    },
+    beforeDestroy() {},
+    methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+#container {
+    width: 100%;
+    height: 100%;
+}
+</style>
