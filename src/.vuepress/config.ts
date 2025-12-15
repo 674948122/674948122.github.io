5a1fc34c96f1d1c94abf5c07684a868000144ac9
pandegong <pandegong1992@gmail.com>
2025-12-15 10:44:08 +0800

    build: 添加 Vite 打包配置并设置 SCSS 和别名选项

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
@@ -6,6 +7,23 @@ export default defineUserConfig({
   base: "/",
   lang: "zh-CN",
   theme,
+  bundler: viteBundler({
+    viteOptions: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            quietDeps: true,
+            silenceDeprecations: ["color-functions", "import"],
+          },
+        },
+      },
+      resolve: {
+        alias: {
+          "cytoscape/dist/cytoscape.umd.js": "cytoscape",
+        },
+      },
+    },
+  }),
 
   // Enable it with pwa
   // shouldPrefetch: false,
